90d8fed36ab86ac944d02594d99256f051133f9d
Joseph Loomis <73568630+Joehl1103@users.noreply.github.com>
2026-06-12 15:56:24 -0500

    tested adding new todo

--- todo-app/todo-frontend/src/Todos/Form.jsx
+++ todo-app/todo-frontend/src/Todos/Form.jsx
@@ -1,23 +1,32 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const TodoForm = ({ createTodo }) => {
-  const [text, setText] = useState('')
+  const [text, setText] = useState("");
 
   const onChange = ({ target }) => {
-    setText(target.value)
-  }
+    setText(target.value);
+  };
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    createTodo({ text })
-  }
+    e.preventDefault();
+    createTodo({ text });
+  };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" name="text" value={text} onChange={onChange} />
-      <button type="submit"> Submit </button>
+    <form onSubmit={handleSubmit} data-testid="form">
+      <input
+        type="text"
+        data-testid="form-input"
+        name="text"
+        value={text}
+        onChange={onChange}
+      />
+      <button type="submit" data-testid="form-button">
+        {" "}
+        Submit{" "}
+      </button>
     </form>
-  )
-}
+  );
+};
 
-export default TodoForm
+export default TodoForm;
--- todo-app/todo-frontend/src/Todos/TodoView.test.jsx
+++ todo-app/todo-frontend/src/Todos/TodoView.test.jsx
@@ -1,4 +1,5 @@
-import { render, screen, logRoles } from "@testing-library/react";
+import { render, screen, logRoles, fireEvent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { describe, it } from "vitest";
 import TodoView from "./TodoView";
 import { todos } from "./fixtures";
@@ -8,24 +9,31 @@ import { setupServer } from "msw/node";
 const baseUrl = import.meta.env.VITE_BACKEND_URL;
 const server = setupServer(
   http.get(`/todos`, () => {
-    console.log("todos", todos);
     return HttpResponse.json(todos);
   }),
-  http.post(`${baseUrl}/todos`, () => {
-    const newTodo = { _id: "3", text: "new todo", done: false };
-    const newTodos = [...todos, newTodo];
-    return HttpResponse.json(newTodos);
+  http.post("/todos", () => {
+    return HttpResponse.json({ _id: "3", text: "new todo", done: false });
   }),
 );
 
 describe("List", () => {
-  beforeAll(() => server.listen());
+  const user = userEvent.setup();
+  beforeAll(() => {
+    server.listen();
+  });
   beforeEach(() => server.resetHandlers());
   afterAll(() => server.close());
   it("renders", async () => {
     render(<TodoView />);
     const all = await screen.findAllByTestId("todo");
-    screen.debug(all);
     expect(all.length).toEqual(2);
+  });
+  it.only("post", async () => {
+    render(<TodoView />);
+    const bttn = screen.getByTestId("form-button");
+    await user.click(bttn);
+    const newTodo = await screen.findByText("new todo");
+    screen.debug();
+    expect(newTodo).toBeTruthy();
   });
 });
